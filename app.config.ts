7493354c7edbe42241907f6c166fe2105eddb2cb
dheerajvarmarsv <dheeraj.rudraraju@cloudfabrix.com>
2025-04-02 14:29:16 -0700

    Add ios_app_id for Google Mobile Ads4

--- app.config.ts
+++ app.config.ts
@@ -27,7 +27,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       UIBackgroundModes: ["remote-notification"],
       NSHealthShareUsageDescription: "CTP needs access to your health data to track your activities like steps, distance, and workouts. This data is used to monitor your progress in challenges and help you achieve your fitness goals. Your data is stored securely and is only used within the app.",
       NSHealthUpdateUsageDescription: "CTP needs permission to update your health data to accurately track your activities and ensure your challenge progress is up to date. This allows us to sync your fitness achievements and provide accurate statistics. Your data privacy and security are our top priority.",
-      GADApplicationIdentifier: "ca-app-pub-6833157133488263~6430444881"
+      GADApplicationIdentifier: "ca-app-pub-6833157133488263~3762282463"
     }
   },
   android: {
@@ -54,6 +54,10 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
     eas: {
       projectId: "ace5179f-9d07-4a93-86cb-af3735bf01ef"
+    },
+    "react-native-google-mobile-ads": {
+      android_app_id: "ca-app-pub-6833157133488263~9820424352",
+      ios_app_id: "ca-app-pub-6833157133488263~3762282463" 
     }
   },
   plugins: [
@@ -66,7 +70,22 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
           compileSdkVersion: 33,
           targetSdkVersion: 33,
           buildToolsVersion: "33.0.0"
+        },
+        ios: {
+          useFrameworks: "static"
         }
+      }
+    ],
+    [
+      "react-native-google-mobile-ads",
+      {
+        android_app_id: "ca-app-pub-6833157133488263~9820424352",
+        ios_app_id: "ca-app-pub-6833157133488263~3762282463",
+        user_tracking_description: "This identifier will be used to deliver personalized ads to you.",
+        delay_app_measurement_init: true,
+        sk_ad_network_items: [
+          "cstr6suwn9.skadnetwork"
+        ]
       }
     ],
     "expo-notifications"
