f2e32fd451645213e1a0fc2546d36a69cf173623
dheerajvarmarsv <dheeraj.rudraraju@cloudfabrix.com>
2025-04-08 12:45:26 -0700

    trying hopefully last time  to fix again healthkit

--- plugins/ios-healthkit-config/index.js
+++ plugins/ios-healthkit-config/index.js
@@ -1,33 +1,26 @@
-const fs = require('fs');
-const path = require('path');
-const { withDangerousMod } = require('@expo/config-plugins');
+const { withAppDelegate } = require("@expo/config-plugins");
 
 module.exports = function withHealthKitConfig(config) {
-  return withDangerousMod(config, [
-    'ios',
-    async (config) => {
-      const appDelegatePath = path.join(config.modRequest.platformProjectRoot, 'AppDelegate.mm');
-      let appDelegateContent = fs.readFileSync(appDelegatePath, 'utf-8');
-
-      // Look for the didFinishLaunchingWithOptions method
-      if (!appDelegateContent.includes('initializeBackgroundObservers')) {
-        // Add the RCTAppleHealthKit import if it doesn't exist
-        if (!appDelegateContent.includes('#import <RCTAppleHealthKit/RCTAppleHealthKit.h>')) {
-          appDelegateContent = appDelegateContent.replace(
-            '#import <React/RCTLinkingManager.h>',
-            '#import <React/RCTLinkingManager.h>\n#import <RCTAppleHealthKit/RCTAppleHealthKit.h>'
-          );
-        }
-        
-        // Add the HealthKit initialization code
-        appDelegateContent = appDelegateContent.replace(
-          'RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];',
-          'RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];\n  // Initialize HealthKit background observers\n  [[RCTAppleHealthKit new] initializeBackgroundObservers:bridge];'
-        );
-      }
-      
-      fs.writeFileSync(appDelegatePath, appDelegateContent);
+  return withAppDelegate(config, async (config) => {
+    if (config.modResults.contents.includes('initializeBackgroundObservers')) {
       return config;
-    },
-  ]);
+    }
+
+    // Add import statement if it doesn't exist
+    const importStatement = `#import <RCTAppleHealthKit/RCTAppleHealthKit.h>`;
+    if (!config.modResults.contents.includes(importStatement)) {
+      config.modResults.contents = config.modResults.contents.replace(
+        '#import <React/RCTLinkingManager.h>',
+        `#import <React/RCTLinkingManager.h>\n${importStatement}`
+      );
+    }
+
+    // Add HealthKit initialization
+    config.modResults.contents = config.modResults.contents.replace(
+      'RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];',
+      'RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];\n  // Initialize HealthKit background observers\n  [[RCTAppleHealthKit new] initializeBackgroundObservers:bridge];'
+    );
+
+    return config;
+  });
 };
